fa7c41356c50725294594526ad51f426ecd8287a
joyanggi <jyg8033@gmail.com>
2023-07-28 02:55:05 +0900

    Test: 브랜치 테스트

--- src/main/java/controller/connectionController.java
+++ src/main/java/controller/connectionController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class connectionController {
 
     @GetMapping("/")
-    public String hello(){
+    public String hello0(){
         return "index";
     }
 }
